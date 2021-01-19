9af1ac1237aae6905f4706ee7acbeee934b15a03
JCash <mwesterdahl76@gmail.com>
2021-01-19 11:01:52 +0100

    Use the unstripped swift libraries

--- facebook/libhelper/ios/update.sh
+++ facebook/libhelper/ios/update.sh
@@ -22,23 +22,25 @@ DST=../../res/ios/Frameworks
 
 mkdir -p $DST
 
-${SDK}/usr/bin/bitcode_strip ${SDK}/usr/lib/swift-5.0/iphoneos/libswiftAVFoundation.dylib -r -o ${DST}/libswiftAVFoundation.dylib
-${SDK}/usr/bin/bitcode_strip ${SDK}/usr/lib/swift-5.0/iphoneos/libswiftAccelerate.dylib -r -o ${DST}/libswiftAccelerate.dylib
-${SDK}/usr/bin/bitcode_strip ${SDK}/usr/lib/swift-5.0/iphoneos/libswiftCore.dylib -r -o ${DST}/libswiftCore.dylib
-${SDK}/usr/bin/bitcode_strip ${SDK}/usr/lib/swift-5.0/iphoneos/libswiftCoreAudio.dylib -r -o ${DST}/libswiftCoreAudio.dylib
-${SDK}/usr/bin/bitcode_strip ${SDK}/usr/lib/swift-5.0/iphoneos/libswiftCoreData.dylib -r -o ${DST}/libswiftCoreData.dylib
-${SDK}/usr/bin/bitcode_strip ${SDK}/usr/lib/swift-5.0/iphoneos/libswiftCoreFoundation.dylib -r -o ${DST}/libswiftCoreFoundation.dylib
-${SDK}/usr/bin/bitcode_strip ${SDK}/usr/lib/swift-5.0/iphoneos/libswiftCoreGraphics.dylib -r -o ${DST}/libswiftCoreGraphics.dylib
-${SDK}/usr/bin/bitcode_strip ${SDK}/usr/lib/swift-5.0/iphoneos/libswiftCoreImage.dylib -r -o ${DST}/libswiftCoreImage.dylib
-${SDK}/usr/bin/bitcode_strip ${SDK}/usr/lib/swift-5.0/iphoneos/libswiftCoreLocation.dylib -r -o ${DST}/libswiftCoreLocation.dylib
-${SDK}/usr/bin/bitcode_strip ${SDK}/usr/lib/swift-5.0/iphoneos/libswiftCoreMedia.dylib -r -o ${DST}/libswiftCoreMedia.dylib
-${SDK}/usr/bin/bitcode_strip ${SDK}/usr/lib/swift-5.0/iphoneos/libswiftDarwin.dylib -r -o ${DST}/libswiftDarwin.dylib
-${SDK}/usr/bin/bitcode_strip ${SDK}/usr/lib/swift-5.0/iphoneos/libswiftDispatch.dylib -r -o ${DST}/libswiftDispatch.dylib
-${SDK}/usr/bin/bitcode_strip ${SDK}/usr/lib/swift-5.0/iphoneos/libswiftFoundation.dylib -r -o ${DST}/libswiftFoundation.dylib
-${SDK}/usr/bin/bitcode_strip ${SDK}/usr/lib/swift-5.0/iphoneos/libswiftMetal.dylib -r -o ${DST}/libswiftMetal.dylib
-${SDK}/usr/bin/bitcode_strip ${SDK}/usr/lib/swift-5.0/iphoneos/libswiftObjectiveC.dylib -r -o ${DST}/libswiftObjectiveC.dylib
-${SDK}/usr/bin/bitcode_strip ${SDK}/usr/lib/swift-5.0/iphoneos/libswiftPhotos.dylib -r -o ${DST}/libswiftPhotos.dylib
-${SDK}/usr/bin/bitcode_strip ${SDK}/usr/lib/swift-5.0/iphoneos/libswiftQuartzCore.dylib -r -o ${DST}/libswiftQuartzCore.dylib
-${SDK}/usr/bin/bitcode_strip ${SDK}/usr/lib/swift-5.0/iphoneos/libswiftUIKit.dylib -r -o ${DST}/libswiftUIKit.dylib
-${SDK}/usr/bin/bitcode_strip ${SDK}/usr/lib/swift-5.0/iphoneos/libswiftos.dylib -r -o ${DST}/libswiftos.dylib
-${SDK}/usr/bin/bitcode_strip ${SDK}/usr/lib/swift-5.0/iphoneos/libswiftsimd.dylib -r -o ${DST}/libswiftsimd.dylib
+# previously used #${SDK}/usr/bin/bitcode_strip ${SDK}/usr/lib/swift-5.0/iphoneos/libswiftAVFoundation.dylib -r -o ${DST}/libswiftAVFoundation.dylib
+# to copy the stripped versions. However, since these are later copied to the /SwiftSupport folder, they need to be unmodified
+cp -v ${SDK}/usr/lib/swift-5.0/iphoneos/libswiftAVFoundation.dylib ${DST}/libswiftAVFoundation.dylib
+cp -v ${SDK}/usr/lib/swift-5.0/iphoneos/libswiftAccelerate.dylib ${DST}/libswiftAccelerate.dylib
+cp -v ${SDK}/usr/lib/swift-5.0/iphoneos/libswiftCore.dylib ${DST}/libswiftCore.dylib
+cp -v ${SDK}/usr/lib/swift-5.0/iphoneos/libswiftCoreAudio.dylib ${DST}/libswiftCoreAudio.dylib
+cp -v ${SDK}/usr/lib/swift-5.0/iphoneos/libswiftCoreData.dylib ${DST}/libswiftCoreData.dylib
+cp -v ${SDK}/usr/lib/swift-5.0/iphoneos/libswiftCoreFoundation.dylib ${DST}/libswiftCoreFoundation.dylib
+cp -v ${SDK}/usr/lib/swift-5.0/iphoneos/libswiftCoreGraphics.dylib ${DST}/libswiftCoreGraphics.dylib
+cp -v ${SDK}/usr/lib/swift-5.0/iphoneos/libswiftCoreImage.dylib ${DST}/libswiftCoreImage.dylib
+cp -v ${SDK}/usr/lib/swift-5.0/iphoneos/libswiftCoreLocation.dylib ${DST}/libswiftCoreLocation.dylib
+cp -v ${SDK}/usr/lib/swift-5.0/iphoneos/libswiftCoreMedia.dylib ${DST}/libswiftCoreMedia.dylib
+cp -v ${SDK}/usr/lib/swift-5.0/iphoneos/libswiftDarwin.dylib ${DST}/libswiftDarwin.dylib
+cp -v ${SDK}/usr/lib/swift-5.0/iphoneos/libswiftDispatch.dylib ${DST}/libswiftDispatch.dylib
+cp -v ${SDK}/usr/lib/swift-5.0/iphoneos/libswiftFoundation.dylib ${DST}/libswiftFoundation.dylib
+cp -v ${SDK}/usr/lib/swift-5.0/iphoneos/libswiftMetal.dylib ${DST}/libswiftMetal.dylib
+cp -v ${SDK}/usr/lib/swift-5.0/iphoneos/libswiftObjectiveC.dylib ${DST}/libswiftObjectiveC.dylib
+cp -v ${SDK}/usr/lib/swift-5.0/iphoneos/libswiftPhotos.dylib ${DST}/libswiftPhotos.dylib
+cp -v ${SDK}/usr/lib/swift-5.0/iphoneos/libswiftQuartzCore.dylib ${DST}/libswiftQuartzCore.dylib
+cp -v ${SDK}/usr/lib/swift-5.0/iphoneos/libswiftUIKit.dylib ${DST}/libswiftUIKit.dylib
+cp -v ${SDK}/usr/lib/swift-5.0/iphoneos/libswiftos.dylib ${DST}/libswiftos.dylib
+cp -v ${SDK}/usr/lib/swift-5.0/iphoneos/libswiftsimd.dylib ${DST}/libswiftsimd.dylib
